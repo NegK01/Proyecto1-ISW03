6c9f381b57eff466389b9a6859faf315604ef8e9
Neg <mauricio.or99@gmail.com>
2025-02-22 17:37:09 -0600

    mao3

--- Proyecto/CduReportes.Designer.cs
+++ Proyecto/CduReportes.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "CduReportes";
-            this.Size = new System.Drawing.Size(700, 429);
+            this.Size = new System.Drawing.Size(700, 432);
             this.ResumeLayout(false);
 
         }
